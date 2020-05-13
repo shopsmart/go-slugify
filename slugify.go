12d686059d1e2d50d23eb90e4cc39f238c85925c
Eric Agnew <wenga.eric@gmail.com>
2020-05-13 14:11:17 -0500

    Add strip step to remove character rather than separate

--- slugify.go
+++ slugify.go
@@ -11,21 +11,24 @@ import (
 // Separator separator between words
 var Separator = "-"
 
-// InvalidCharReplacement replaces invalid characters
-var InvalidCharReplacement = ""
+// StripRepl replaces invalid characters
+var StripRepl = ""
 
 // SeparatorForRe for regexp
 var SeparatorForRe = regexp.QuoteMeta(Separator)
 
+// StripChar removes invalid slug characters
+var StripChar = regexp.MustCompile(fmt.Sprintf("['\"]", StripRepl))
+
 // ReInValidChar match invalid slug string
-var ReInValidChar = regexp.MustCompile(fmt.Sprintf("[^%sa-zA-Z0-9]", SeparatorForRe))
+var ReInValidChar = regexp.MustCompile(fmt.Sprintf("[^%sa-zA-Z0-9]", Separator))
 
 // ReDupSeparatorChar match duplicate separator string
 var ReDupSeparatorChar = regexp.MustCompile(fmt.Sprintf("%s{2,}", SeparatorForRe))
 
 // Version return version
 func Version() string {
-	return "0.2.1"
+	return "0.2.2"
 }
 
 // Slugify implements make a pretty slug from the given text.
@@ -36,10 +39,16 @@ func Slugify(s string) string {
 
 func slugify(s string) string {
 	s = unidecode.Unidecode(s)
-	s = replaceInValidCharacter(s, InvalidCharReplacement)
+	s = StripChar(s, StripRepl)
+	s = replaceInValidCharacter(s, Separator)
 	s = removeDupSeparator(s)
 	s = strings.Trim(s, Separator)
 	s = strings.ToLower(s)
+	return s
+}
+
+func stripChar(s, repl string) string {
+	s = StripChar.ReplaceAllString(s, repl)
 	return s
 }
 
